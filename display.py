a022af201205a932ffd2359238493859a083644e
Shennyfac31 <32761999+Shennyfac31@users.noreply.github.com>
2018-01-03 10:40:18 +0800

    Add files via upload

--- display.py
+++ display.py
@@ -8,12 +8,61 @@ Displayer
 import numpy as np
 import matplotlib.pyplot as plt
 
+def load_back():
+    card_back = {}
+    card_back['normal'] = plt.imread('./images/back.png')
+    card_back['normal_s'] = plt.imread('./images/select.png')
+    card_back['void'] = np.zeros(card_back['normal'].shape)
+    card_back['void_s'] = plt.imread('./images/select_void.png')
+    card_back['mischief'] = plt.imread('./images/mischief.png')
+    return card_back
+
+def load_face():
+    card_face = {}
+    for element in ['fire','air','water','earth','special']:
+        list_pt = []
+        addr = './images/'+element
+        for i in range(5):
+            list_pt.append(plt.imread(addr+str(i+1)+'.png'))
+        card_face[element] = list_pt
+    return card_face
+
+def load_action():
+    normal = {'flip': plt.imread('./images/flip.png'), 
+              'tool': plt.imread('./images/tool.png'), 
+              'hint': plt.imread('./images/hint.png'), 
+              'quit': plt.imread('./images/quit.png')}
+    select = {'flip': plt.imread('./images/flip_s.png'), 
+              'tool': plt.imread('./images/tool_s.png'), 
+              'hint': plt.imread('./images/hint_s.png'), 
+              'quit': plt.imread('./images/quit_s.png')}
+    action = {'normal': normal, 
+              'select': select}
+    return action
+
+def load_tool():
+    normal = {'out': plt.imread('./images/out.png'), 
+              'frog': plt.imread('./images/frog.png'), 
+              'bomb': plt.imread('./images/bomb.png'), 
+              'eye': plt.imread('./images/eye.png'), 
+              'rabbit': plt.imread('./images/rabbit.png'), 
+              'shield': plt.imread('./images/shield.png')}
+    select = {'out': plt.imread('./images/out_s.png'), 
+              'frog': plt.imread('./images/frog_s.png'), 
+              'bomb': plt.imread('./images/bomb_s.png'), 
+              'eye': plt.imread('./images/eye_s.png'), 
+              'rabbit': plt.imread('./images/rabbit_s.png'), 
+              'shield': plt.imread('./images/shield_s.png')}
+    tool = {'normal': normal, 
+            'select': select}
+    return tool
+
 class Display:
     def __init__(self):
         background = plt.imread('./images/bg.png')
         height,width,channel = background.shape
         dpi = plt.rcParams['figure.dpi']
-        fig_size = height/dpi*60, width/dpi*60
+        fig_size = height/dpi, width/dpi
         plt.figure(figsize=fig_size)
         ax = plt.axes([0,0,1,1], frameon=False)
         ax.set_axis_off()
@@ -21,44 +70,66 @@ class Display:
         ax.set_ylim(0,10)
         ax.imshow(background, origin='upper', extent=[0,10,0,10])
         
+        self.card_backs = load_back()
+        self.card_faces = load_face()
         self.cards = []
         for i in range(5):
             rows = []
             for j in range(5):
-                item =  plt.imread('./images/back.png')
-                x1 = 3.0+i*1.5#2.5+i*1.5
-                x2 = 3.5+i*1.5#4.0+i*1.5
-                y1 = 6.3-j*1.5#6.0-j*1.5
-                y2 = 7.2-j*1.5#7.5-j*1.5
+                item = self.card_backs['normal']
+                x1 = 3.0+i*1.5
+                x2 = 3.5+i*1.5
+                y1 = 6.3-j*1.5
+                y2 = 7.2-j*1.5
                 rows.append(ax.imshow(item, origin='upper', extent=[x1,x2,y1,y2]))
             self.cards.append(rows)
         
+        self.action_bar = load_action()
+        self.action_list = ['flip','tool','hint','quit']
         self.ations = []
+        for j in range(4):
+            key = self.action_list[j]
+            item = self.action_bar['normal'][key]
+            x1 = 0.5
+            x2 = 2.5
+            y1 = 6.3-j*1.0
+            y2 = 7.0-j*1.0
+            self.ations.append(ax.imshow(item, origin='upper', extent=[x1,x2,y1,y2]))
+        
+        self.tool_bar = load_tool()
+        self.tool_hide = np.zeros((10,10,4))
+        self.tools = []
+        for i in range(6):
+            if i==0:
+                item = self.tool_bar['normal']['out']
+            else:
+                item = self.tool_hide
+            x1 = 0.0+i*1.0
+            x2 = 1.0+i*1.0
+            y1 = 9.0
+            y2 = 10.0
+            self.tools.append(ax.imshow(item, origin='upper', extent=[x1,x2,y1,y2]))
     
-    def show_card(self,row,col,face):
-        self.cards[row][col].set_data(face)
+    def show_card_face(self, row, col, element, point):
+        self.cards[row][col].set_data(self.card_faces[element][point])
     
+    def show_card_back(self, row, col, status):
+        self.cards[row][col].set_data(self.card_backs[status])
     
-'''
-background = plt.imread('../bg.png')
-height,width,channel = background.shape
+    def select_action(self, index):
+        key = self.action_list[index]
+        self.ations[index].set_data(self.action_bar['select'][key])
+    
+    def unselect_action(self, index):
+        key = self.action_list[index]
+        self.ations[index].set_data(self.action_bar['normal'][key])
+    
+    def show_tool(self, index, status, tool):
+        self.tools[index].set_data(self.tool_bar[status][tool])
+    
+    def hide_tool(self, index):
+        self.tools[index].set_data(self.tool_hide)
+    
+    def delay(self, sec=1):
+        plt.pause(sec)
 
-item = plt.imread('../card_back.png')
-
-dpi = plt.rcParams['figure.dpi']
-fig_size = height/dpi*50, width/dpi*50
-
-plt.figure(figsize=fig_size)
-ax = plt.axes([0,0,1,1], frameon=False)
-ax.set_axis_off()
-ax.set_xlim(0,10)
-ax.set_ylim(0,10)
-im1 = ax.imshow(background, origin='upper', extent=[0,10,0,10])
-im2 = ax.imshow(item, origin='upper', extent=[0,1.5,0,1.5])
-
-plt.pause(1)
-#new = plt.imread('../stereo.png')
-new = np.zeros(item.shape)
-im2.set_data(new)
-'''
-#screen = Display()
--- game.py
+++ game.py
@@ -6,32 +6,9 @@ Play game
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 
 import display
-
-def load_back():
-    card_back = {}
-    card_back['normal'] = plt.imread('./images/back.png')
-    card_back['select'] = plt.imread('./images/select.png')
-    card_back['frog'] = plt.imread('./images/frog.png')
-    card_back['fire'] = plt.imread('./images/fire.png')
-    card_back['air'] = plt.imread('./images/air.png')
-    card_back['water'] = plt.imread('./images/water.png')
-    card_back['earth'] = plt.imread('./images/earth.png')
-    card_back['none'] = np.zeros(card_back['normal'].shape)
-    return card_back
-
-def load_face():
-    card_face = {}
-    for element in ['fire','air','water','earth','special']:
-        list_pt = []
-        addr = './images/'+element
-        for i in range(5):
-            list_pt.append(plt.imread(addr+str(i+1)+'.png'))
-        card_face[element] = list_pt
-    return card_face
 
 class Card:
     def __init__(self, ele, pt):
@@ -41,21 +18,327 @@ class Card:
 
 class Game:
     def __init__(self):
-        self.card_backs = load_back()
-        self.card_faces = load_face()
         deck = []
         for element in ['fire','air','water','earth','special']:
             for i in range(5):
                 deck.append(Card(element,i))
         random.shuffle(deck)
         self.deck = np.array(deck).reshape((5,5))
+        self.menu = ['flip','tool','hint','quit']
+        self.tools = ['out']
+        self.stk = ['eye','frog','rabbit','bomb','shield']
         self.screen = display.Display()
-        
-    def view_all(self):
+        self.state = 'initial'
+        self.action = None
+        self.flipped_row = None
+        self.flipped_col = None
+        self.end_game = False
+        self.score = 0
+        self.bonus = 0
+        self.malus = 0
+        self.hint_count = 0
+        self.menu_ptr = 0
+        self.tools_ptr = 0
+        self.row_ptr = 0
+        self.col_ptr = 0
+        self.counter = 0
+    
+    def view_all(self, odd=None):
         for i in range(5):
             for j in range(5):
-                element = self.deck[i,j].element
-                point = self.deck[i,j].point
-                face = self.card_faces[element][point]
-                self.screen.show_card(i, j, face)
+                if odd is None:
+                    if self.deck[i,j].status=='normal':
+                        self.screen.show_card_face(i, j, self.deck[i,j].element, 
+                                                   self.deck[i,j].point)
+                else:
+                    if (i+j)%2==odd and self.deck[i,j].status=='normal':
+                        self.screen.show_card_face(i, j, self.deck[i,j].element, 
+                                                   self.deck[i,j].point)
+    
+    def hide_all(self):
+        for i in range(5):
+            for j in range(5):
+                self.screen.show_card_back(i, j, self.deck[i,j].status)
+    
+    def control(self, command):
+        if self.state == 'initial':
+            self.step_initial(command)
+        elif self.state == 'menu':
+            self.step_menu(command)
+        elif self.state == 'flip':
+            self.step_flip(command)
+        elif self.state == 'tool':
+            self.step_tool(command)
+        elif self.state == 'hint':
+            self.step_hint(command)
+        else:
+            print('Error: No game state')
+    
+    def step_initial(self, move):
+        if move==1 or move==3:
+            self.menu_ptr = 3
+            self.screen.select_action(self.menu_ptr)
+            self.state = 'menu'
+        elif move==2 or move==4:
+            self.screen.select_action(self.menu_ptr)
+            self.state = 'menu'
+        elif move==0:
+            print('Try something else')
+        else:
+            print('Error: Command error')
+    
+    def step_menu(self, move):
+        if move==1 or move==3:
+            self.screen.unselect_action(self.menu_ptr)
+            self.menu_ptr = (self.menu_ptr+3)%4
+            self.screen.select_action(self.menu_ptr)
+            self.state = 'menu'
+        elif move==2 or move==4:
+            self.screen.unselect_action(self.menu_ptr)
+            self.menu_ptr = (self.menu_ptr+1)%4
+            self.screen.select_action(self.menu_ptr)
+            self.state = 'menu'
+        elif move==0:
+            self.screen.unselect_action(self.menu_ptr)
+            self.state = self.menu[self.menu_ptr]
+            if self.state == 'quit':
+                self.end_game = True
+            elif self.state == 'tool':
+                self.tools_ptr = 0
+                self.screen.show_tool(self.tools_ptr, 'select', self.tools[self.tools_ptr])
+            else:
+                self.row_ptr = 0
+                self.col_ptr = 0
+                self.deck[self.row_ptr,self.col_ptr].status += '_s'
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+        else:
+            print('Error: Command error')
+    
+    def step_flip(self, move):
+        if self.flipped_row is None:
+            if move==0:
+                if self.deck[self.row_ptr,self.col_ptr].status=='void_s':
+                    print('This one is done')
+                    self.state = 'flip'
+                elif self.deck[self.row_ptr,self.col_ptr].element=='special':
+                    p = self.deck[self.row_ptr,self.col_ptr].point
+                    self.tools.append(self.stk[p])
+                    idx = len(self.tools)-1
+                    self.screen.show_tool(idx, 'normal', self.tools[idx])
+                    self.screen.show_card_face(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].element, 
+                                               self.deck[self.row_ptr,self.col_ptr].point)
+                    self.screen.delay()
+                    self.deck[self.row_ptr,self.col_ptr].status = 'void'
+                    self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].status)
+                    self.counter += 1
+                    if self.counter==25:
+                        self.end_game = True
+                    self.state = 'menu'
+                    self.menu_ptr = 0
+                    self.screen.select_action(self.menu_ptr)
+                else:
+                    self.flipped_row = self.row_ptr
+                    self.flipped_col = self.col_ptr
+                    self.screen.show_card_face(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].element, 
+                                               self.deck[self.row_ptr,self.col_ptr].point)
+                    self.state = 'flip'
+            else:
+                self.choose_card(move)
+                self.state = 'flip'
+        else:
+            if move==0:
+                if (self.flipped_row == self.row_ptr 
+                and self.flipped_col == self.col_ptr):
+                    print('Cannot choose the same card')
+                    self.state = 'flip'
+                elif self.deck[self.row_ptr,self.col_ptr].status=='void_s':
+                    print('This one is done')
+                    self.state = 'flip'
+                elif self.deck[self.row_ptr,self.col_ptr].element=='special':
+                    self.screen.show_card_face(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].element, 
+                                               self.deck[self.row_ptr,self.col_ptr].point)
+                    self.screen.delay()
+                    self.deck[self.row_ptr,self.col_ptr].status = 'void'
+                    self.deck[self.flipped_row,self.flipped_col].status = 'normal'
+                    self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].status)
+                    self.screen.show_card_back(self.flipped_row, self.flipped_col, 
+                                               self.deck[self.flipped_row,self.flipped_col].status)
+                    self.flipped_row = None
+                    p = self.deck[self.row_ptr,self.col_ptr].point
+                    self.special_card(self.stk[p])
+                    self.counter += 1
+                    if self.counter==25:
+                        self.end_game = True
+                    self.state = 'menu'
+                    self.menu_ptr = 0
+                    self.screen.select_action(self.menu_ptr)
+                else:
+                    self.screen.show_card_face(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].element, 
+                                               self.deck[self.row_ptr,self.col_ptr].point)
+                    self.screen.delay()
+                    if (self.deck[self.flipped_row,self.flipped_col].point 
+                    == self.deck[self.row_ptr,self.col_ptr].point):
+                        self.score += (10+self.bonus*10)
+                        self.bonus = self.bonus//2
+                        self.deck[self.flipped_row,self.flipped_col].status = 'void'
+                        self.deck[self.row_ptr,self.col_ptr].status = 'void'
+                        self.counter += 2
+                        if self.counter==25:
+                            self.end_game = True
+                    else:
+                        self.score -= (1+self.malus*3)
+                        self.malus = self.malus//2
+                        self.deck[self.flipped_row,self.flipped_col].status = 'normal'
+                        self.deck[self.row_ptr,self.col_ptr].status = 'normal'
+                    self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].status)
+                    self.screen.show_card_back(self.flipped_row, self.flipped_col, 
+                                               self.deck[self.flipped_row,self.flipped_col].status)
+                    self.flipped_row = None
+                    self.state = 'menu'
+                    self.menu_ptr = 0
+                    self.screen.select_action(self.menu_ptr)
+            else:
+                self.choose_card(move)
+                self.state = 'flip'
+    
+    def step_hint(self, move):
+        self.hint_count = (self.hint_count+1)%5
+        if move==0:
+            if self.deck[self.row_ptr,self.col_ptr].status=='void_s':
+                print('This one is done')
+                self.state = 'hint'
+            else:
+                if self.hint_count==0:
+                    self.screen.show_card_back(self.row_ptr, self.col_ptr, 'mischief')
+                else:
+                    self.screen.show_card_face(self.row_ptr, self.col_ptr, 
+                                               self.deck[self.row_ptr,self.col_ptr].element, 
+                                               self.deck[self.row_ptr,self.col_ptr].point)
+                self.screen.delay()
+                self.deck[self.row_ptr,self.col_ptr].status = 'normal'
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+                self.score -= 1
+                self.state = 'menu'
+                self.menu_ptr = 0
+                self.screen.select_action(self.menu_ptr)
+        else:
+            self.choose_card(move)
+            self.state = 'hint'
+    
+    def step_tool(self, move):
+        if move==1 or move==3:
+            num = len(self.tools)
+            self.screen.show_tool(self.tools_ptr, 'normal', self.tools[self.tools_ptr])
+            self.tools_ptr = (self.tools_ptr+num-1)%num
+            self.screen.show_tool(self.tools_ptr, 'select', self.tools[self.tools_ptr])
+            self.state = 'tool'
+        elif move==2 or move==4:
+            num = len(self.tools)
+            self.screen.show_tool(self.tools_ptr, 'normal', self.tools[self.tools_ptr])
+            self.tools_ptr = (self.tools_ptr+1)%num
+            self.screen.show_tool(self.tools_ptr, 'select', self.tools[self.tools_ptr])
+            self.state = 'tool'
+        elif move==0:
+            if self.tools_ptr>0:
+                selected_tool = self.tools.pop(self.tools_ptr)
+                if selected_tool=='eye':
+                    self.view_all()
+                    self.screen.delay()
+                    self.hide_all()
+                elif selected_tool=='frog':
+                    self.view_all(odd=1)
+                    self.screen.delay()
+                    self.hide_all()
+                elif selected_tool=='rabbit':
+                    self.view_all(odd=0)
+                    self.screen.delay()
+                    self.hide_all()
+                elif selected_tool=='bomb':
+                    self.bonus = 8
+                elif selected_tool=='shield':
+                    self.malus = 0
+            for i in range(self.tools_ptr,len(self.tools)):
+                self.screen.show_tool(self.tools_ptr, 'normal', self.tools[self.tools_ptr])
+            self.screen.hide_tool(len(self.tools))
+            self.state = 'menu'
+            self.menu_ptr = 0
+            self.screen.select_action(self.menu_ptr)
+        else:
+            print('Error: Command error')
+    
+    def choose_card(self, move):
+        if move==3:
+            st = self.deck[self.row_ptr,self.col_ptr].status
+            self.deck[self.row_ptr,self.col_ptr].status = st.replace('_s','')
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+            self.row_ptr = (self.row_ptr+4)%5
+            self.deck[self.row_ptr,self.col_ptr].status += '_s'
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+        elif move==4:
+            st = self.deck[self.row_ptr,self.col_ptr].status
+            self.deck[self.row_ptr,self.col_ptr].status = st.replace('_s','')
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+            self.row_ptr = (self.row_ptr+1)%5
+            self.deck[self.row_ptr,self.col_ptr].status += '_s'
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+        elif move==1:
+            st = self.deck[self.row_ptr,self.col_ptr].status
+            self.deck[self.row_ptr,self.col_ptr].status = st.replace('_s','')
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+            self.col_ptr = (self.col_ptr+4)%5
+            self.deck[self.row_ptr,self.col_ptr].status += '_s'
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+        elif move==2:
+            st = self.deck[self.row_ptr,self.col_ptr].status
+            self.deck[self.row_ptr,self.col_ptr].status = st.replace('_s','')
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+            self.col_ptr = (self.col_ptr+1)%5
+            self.deck[self.row_ptr,self.col_ptr].status += '_s'
+            if (self.flipped_row != self.row_ptr or self.flipped_col != self.col_ptr):
+                self.screen.show_card_back(self.row_ptr, self.col_ptr, 
+                                           self.deck[self.row_ptr,self.col_ptr].status)
+        else:
+            print('Error: Choose card error')
+    
+    def special_card(self, card_act):
+        if card_act=='eye':
+            self.deck = self.deck.T
+            self.hide_all()
+        elif card_act=='frog':
+            np.random.shuffle(self.deck)
+            self.hide_all()
+        elif card_act=='rabbit':
+            self.deck = self.deck.T
+            np.random.shuffle(self.deck)
+            self.deck = self.deck.T
+            self.hide_all()
+        elif card_act=='bomb':
+            self.malus = 8
+        elif card_act=='shield':
+            self.bonus = 0
+        else:
+            print('Error: Special card unfound')
 
