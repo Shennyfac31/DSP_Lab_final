f4c509c59c07d76ec12c06e56e9e74a4a928243f
Shennyfac31 <32761999+Shennyfac31@users.noreply.github.com>
2018-01-07 22:36:04 +0800

    Add files via upload

--- final.py
+++ final.py
@@ -15,10 +15,10 @@ import game
 fs = gv.RATE
 
 core = game.Game()
-txt = plt.text(1, 1, 'Ready? The recording will start in 5 sec.', fontsize=25, color='r')
-core.screen.delay(1)
-for i in range(4):
-    txt.set_text('Ready? The recording will start in '+str(4-i)+' sec.')
+txt = plt.text(1, 1, '', fontsize=25, color='r')
+core.screen.delay(0.1)
+for i in range(5):
+    txt.set_text('Ready? The recording will start in '+str(5-i)+' sec.')
     core.screen.delay(1)
 
 txt.set_text('')
@@ -42,11 +42,21 @@ core.screen.delay(0.01)
 
 while core.end_game is False:
     frames = gv.get_digit()
-    digit,dis = rcg.test(frames,fs,template)
-    print((digit,dis))
-    core.control(digit)
-    txt.set_text('Score: '+str(core.score))
-    core.screen.delay(0.01)
+    
+    if len(frames)>fs*5:
+        dis = 3000
+    else:
+        digit,dis = rcg.test(frames,fs,template)
+        print((digit,dis))
+    
+    if dis>2000:
+        core.txt.set_text('What\'s so funny?')
+        core.screen.delay(0.5)
+        core.txt.set_text('')
+    else:
+        core.control(digit)
+        txt.set_text('Score: '+str(core.score))
+        core.screen.delay(0.01)
 
 core.ending()
 txt.set_position((5,2))
